957e9b636441dbe216d987b4526cd84de0c50712
godfrey147 <Godfrey.oteikwu@kodehauz.com>
2026-03-30 11:57:31 +0100

    update about section

---
+++ scripts/about.js
@@ -0,0 +1,11 @@
+window.addEventListener('scroll', () => {
+  const aboutImageContainer = document.querySelector('.about-image-container');
+  console.log(aboutImageContainer);
+  let scrollY = window.scrollY;
+
+  let scale = 1 + scrollY / 1000;
+
+  scale = Math.min(scale, 1.5);
+
+  aboutImageContainer.style.transform = `scale(${scale})`;
+});
