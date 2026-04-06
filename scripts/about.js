98c0cf89d5005118af3fbfd147a3eb704ae9d0e3
oteikwu1 <oteikwuon@gmail.com>
2026-04-06 03:45:19 +0100

    about mobile version update

--- scripts/about.js
+++ scripts/about.js
@@ -3,8 +3,13 @@ window.addEventListener('scroll', () => {
   let scrollY = window.scrollY;
 
   let scale = 1 + scrollY / 1000;
+  console.log(scale)
 
   scale = Math.min(scale, 1.5);
 
   aboutImageContainer.style.transform = `scale(${scale})`;
 });
+
+const names = ['john', 'jane', 'james', 'ane'];
+const obj = {...names}
+console.log(names)
